ee888c6cadba5c09afa10f357cc06e2766eacb90
cesar <cesar.moreno04@uptc.edu.co>
2023-09-27 19:44:02 -0500

    SAC-57 | Add calendar component

--- src/components/ComCalendar/ComCalendar.tsx
+++ src/components/ComCalendar/ComCalendar.tsx
@@ -1,55 +1,59 @@
 "use client";
-import React, { useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 import styles from './styles.module.css';
 
 type Appointment = {
-    title: string;
-    start: string;
-    end: string;
-    cliente: string;
-    tatuador: string;
-    state: "sin pagar" | "pagado" | "abonado";
-}
+    sessionId: string;
+    createdAt: string;
+    date: string;
+    estimatedTime: number;
+    status: string;
+    price: string;
+    appointmentIdFk: string;
+    description: string;
+};
+
+const appointments: Appointment[] = [
+    {
+        sessionId: "123",
+        createdAt: "2023-09-26T09:00:00",
+        date: "2023-09-28T10:00:00",
+        estimatedTime: 60,
+        status: "sin pagar",
+        price: "50",
+        appointmentIdFk: "456",
+        description: "Cita para césar",
+    },
+    {
+        sessionId: "1234",
+        createdAt: "2023-09-26T09:00:00",
+        date: "2023-09-28T10:00:00",
+        estimatedTime: 60,
+        status: "sin pagar",
+        price: "50",
+        appointmentIdFk: "456",
+        description: "Cita para césar 2",
+    },
+    {
+        sessionId: "12324",
+        createdAt: "2023-09-26T12:00:00",
+        date: "2023-09-28T22:00:00",
+        estimatedTime: 60,
+        status: "pagado",
+        price: "50",
+        appointmentIdFk: "456",
+        description: "Cita para césar 3",
+    },
+];
 
 function ComCalendar() {
-    const [appointments, setAppointments] = useState([
-        {
-            title: "Cita existente",
-            start: "2023-09-30T10:00:00",
-            end: "2023-09-30T11:00:00",
-            cliente: "César",
-            tatuador: "César también",
-            state: "sin pagar",
-        },
-        {
-            title: "Prueba de cita 2",
-            start: "2023-09-30T10:00:00",
-            end: "2023-09-30T11:00:00",
-            cliente: "Augusto",
-            tatuador: "Augusto artista",
-            state: "abonado",
-        },
-        {
-            title: "Prueba de cita 3",
-            start: "2023-09-30T08:00:00",
-            end: "2023-09-30T12:00:00",
-            cliente: "Augusto",
-            tatuador: "Augusto artista",
-            state: "pagado",
-        },
-        {
-            title: "Cita césar",
-            start: "2023-09-26T09:00:00",
-            end: "2023-09-26T11:00:00",
-            cliente: "César",
-            tatuador: "César también",
-            state: "pagado",
-        },
-    ]);
+    const calendarRef = useRef<FullCalendar | null>(null);
+    const [selectedEvent, setSelectedEvent] = useState<Appointment | null>(null)
 
     const getColorForState = (state: string): string => {
         switch (state) {
@@ -64,23 +68,68 @@ function ComCalendar() {
         }
     };
 
+    const handleClosePopup = () => {
+        setSelectedEvent(null);
+    }
+
+    useEffect(() => {
+        const headerButtons = document.querySelectorAll(".fc-header-toolbar button");
+        if (headerButtons) {
+            headerButtons.forEach((button) => {
+                (button as HTMLElement).style.backgroundColor = "#78160C";
+                (button as HTMLElement).style.borderColor = "black";
+            });
+        }
+    }, []);
+
     return (
         <div className={styles.fullCalendar}>
             <FullCalendar
-                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                plugins={[dayGridPlugin, listPlugin, timeGridPlugin, interactionPlugin]}
                 initialView={"dayGridMonth"}
-                headerToolbar={{
+                headerToolbar ={{
                     start: "today prev,next",
                     center: "title",
-                    end: "dayGridMonth,timeGridWeek,timeGridDay",
+                    end: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+                }}
+                buttonText={{
+                    today: "Today",
+                    month: "Month",
+                    week: "Week",
+                    day: "Day",
+                    list: "List"
                 }}
                 height={"80vh"}
-                events={appointments.map((appointment) => ({
-                    ...appointment,
-                    color: getColorForState(appointment.state),
+
+                events={appointments.map(appointment => ({
+                    id: appointment.sessionId,
+                    title: appointment.description,
+                    start: new Date(appointment.date),
+                    end: new Date(new Date(appointment.date).getTime() + appointment.estimatedTime * 60000),
+                    color: getColorForState(appointment.status)
                 }))}
+                eventClick={(clickInfo) => {
+                    const event = appointments.find(appointment => appointment.sessionId === clickInfo.event.id);
+                    if (event) {
+                        console.log(event)
+                        setSelectedEvent(event);
+                    }
+                }}
             />
+            {selectedEvent && (
+                <div className={styles.popup}>
+                    <div className={styles.popupContent}>
+                        <h2>{selectedEvent.description}</h2>
+                        <p>Start: {new Date(selectedEvent.date).toLocaleString()}</p>
+                        <p>End: {new Date(new Date(selectedEvent.date).getTime() + selectedEvent.estimatedTime * 60000).toLocaleString()}</p>
+                        <p>Status: {selectedEvent.status}</p>
+                        <p>Price: {selectedEvent.price}</p>
+                        <button onClick={handleClosePopup}>Cerrar</button>
+                    </div>
+                </div>
+            )}
         </div>
+
     );
 }
 
