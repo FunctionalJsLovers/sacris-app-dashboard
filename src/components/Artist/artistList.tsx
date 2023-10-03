43de2653b6b8887f89b079a679ad29cf26679346
xArkqngel <arkqngel343@gmail.com>
2023-10-03 16:30:22 -0500

    fix artistId typo

--- src/components/Artist/artistList.tsx
+++ src/components/Artist/artistList.tsx
@@ -35,7 +35,7 @@ function Card({ }) {
           <div className={styles.artistContainer}>
             {artists?.map((artist:any)=>(
             // eslint-disable-next-line react/jsx-key
-          <div className={styles.card} key={artist.adminId}>
+          <div className={styles.card} key={artist.artistId}>
             <div className={styles.carddetails} >
                 <div className={styles.cardImage}>
                   <Image src="https://s3-alpha-sig.figma.com/img/b940/caf9/f3a52bcc9317c793ebc094db911b237b?Expires=1696204800&Signature=orXUC1z2Ieogp1Td5CEEycHwZ06VwuUC-F-Y-6bgjerrygHoo6Cu0B4quPh9WSjeeilJtPm5aR385jaarT5-X0HTNHyMtAKGox7f8CM~Cj2~nGF4GxOzqjLptqZ32yNZIpj4afhEhNou4KLaaKqkE1RSu4hlWZSCFjMxASCDkjf5Ojv97PdMIesCgPDdYJo3Y2UMDvItteqVGjvv3hk-sG28gUuzbsuDyuxfXfKRLPZyJRHf1TRZynWHTfipHTmIPvBzvVMSb3gRqth6JcIQLKYEeO~3Zy5MyD-64mUzOvDNbGedlM8IBXZuj6wNm6h3pLOa-E4aMK55CQcV2u5zXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt={`${artist.name}'s Avatar`} width={15} height={15} />
@@ -49,7 +49,7 @@ function Card({ }) {
                   <RiInboxLine className={styles.product_icon} title="Ver Productos" />
                   <RiCalendarTodoLine className={styles.calendar_icon} title="Ver Citas" />
                   <RiExternalLinkLine className={styles.portfolio_icon} title="Ver Portafolio" />
-                  <RiFileUserLine className={styles.edit_icon} title="Editar Perfil" onClick={() => handleEditIconClick(artist.adminId)}/>
+                  <RiFileUserLine className={styles.edit_icon} title="Editar Perfil" onClick={() => handleEditIconClick(artist.artistId)}/>
                 </div>
           </div>
           ))}
