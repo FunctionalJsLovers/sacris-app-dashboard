1c874f1cfd4bed027330beeb9295bcea199b0dbb
Edwin Sichaca <edwin.sichaca@uptc.edu.co>
2023-10-23 18:26:02 -0500

    Create Client

--- src/services/ClientApi.ts
+++ src/services/ClientApi.ts
@@ -2,3 +2,19 @@ export async function getAllClients() {
   const response = await fetch('http://18.237.102.202:9000/admin/clients/');
   return await response.json();
 }
+
+export async function createClient(clientData: any) {
+  const response = await fetch('http://18.237.102.202:9000/admin/clients/', {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(clientData),
+  });
+  if (!response.ok) {
+    const errorResponse = await response.text();
+    throw new Error(errorResponse);
+  }
+  const user = await response.json();
+  return user;
+}
