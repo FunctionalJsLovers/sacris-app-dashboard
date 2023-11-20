4d540528c46854a9976aacff18a6e3f2ff8fd790
xArkqngel <arkqngel343@gmail.com>
2023-11-20 17:26:06 -0500

    fetch fixes

--- src/app/(admin)/appointments/page.tsx
+++ src/app/(admin)/appointments/page.tsx
@@ -8,6 +8,8 @@ import CreateAppointment from '@/components/Appointment/ManageAppointments/Creat
 import { Icon } from '@iconify/react/dist/iconify.js';
 import Default from '@/components/Default/Default';
 import Sessions from '@/components/Sessions/ManageSessions/Sessions';
+import { useQuery } from 'react-query';
+import { getAllAppointments } from '@/services/AppointmentAPI';
 
 interface Appointment {
   id: string;
@@ -19,7 +21,9 @@ interface Appointment {
 }
 
 const Appointments: React.FC = () => {
-  const [allAppointments, setAllAppointments] = useState<Appointment[]>([]);
+  const [allAppointments, setAllAppointments] = useState<
+    Appointment[] | undefined
+  >([]);
   const [filteredAppointments, setFilteredAppointments] = useState<
     Appointment[]
   >([]);
@@ -30,65 +34,34 @@ const Appointments: React.FC = () => {
   const [editWithoutId, setEditWithoutId] = useState(false);
   const [deleteWithoutId, setDeleteWithoutId] = useState(false);
   const [showCreateAppointment, setShowCreateAppointment] = useState(false);
-  const [showEditAppointment, setShowEditAppointment] = useState(false);
-  const [showDeleteAppointment, setShowDeleteAppointment] = useState(false);
   const [showSessionContent, setShowSessionContent] = useState(false);
   const [isEditing, setIsEditing] = useState(true);
   const [editMode, setIsEditMode] = useState(false);
   const [deleteMode, setIsDeleteMode] = useState(false);
   const [defaultMode, setIsDefaultMode] = useState(true);
-  const apiBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-  useEffect(() => {
-    axios
-      .get(`${apiBaseUrl}/admin/appointments/`)
-      .then((response) => {
-        if (Array.isArray(response.data.appointments)) {
-          setAllAppointments(response.data.appointments);
-        } else {
-          console.error(
-            'La respuesta no contiene una matriz de citas válida:',
-            response.data,
-          );
-        }
-      })
-      .catch((error) => {
-        console.error('Error al cargar las citas:', error);
-      });
-  }, []);
+  const { data: appointments, refetch } = useQuery({
+    queryKey: ['artists'],
+    queryFn: getAllAppointments,
+    refetchOnWindowFocus: false,
+  });
 
   const reloadAppointments = () => {
-    axios
-      .get(`${apiBaseUrl}/admin/appointments/`)
-      .then((response) => {
-        if (Array.isArray(response.data.appointments)) {
-          setAllAppointments(response.data.appointments);
-        } else {
-          console.error('No se encontró citas válidas:', response.data);
-        }
-      })
-      .catch((error) => {
-        console.error('Error al cargar las citas:', error);
-      });
+    refetch();
   };
 
   useEffect(() => {
-    reloadAppointments();
-  }, [reloadAppointments()]);
-
-  useEffect(() => {
-    const filtered = allAppointments.filter((appointment) =>
-      appointment.identifier.toLowerCase().includes(searchTerm.toLowerCase()),
+    const filtered = appointments?.appointments.filter(
+      (appointment: Appointment) =>
+        appointment.identifier.toLowerCase().includes(searchTerm.toLowerCase()),
     );
-    setFilteredAppointments(filtered);
-  }, [searchTerm, allAppointments]);
+    setFilteredAppointments(filtered || []);
+  }, [searchTerm, appointments]);
 
   const clearAll = () => {
     setShowSessionContent(false);
-    setShowEditAppointment(false);
     setShowCreateAppointment(false);
     setEditWithoutId(false);
-    setShowDeleteAppointment(false);
     setDeleteWithoutId(false);
     setIsDefaultMode(false);
     clearModes();
@@ -102,7 +75,6 @@ const Appointments: React.FC = () => {
     clearAll();
     setIsEditing(true);
     setIsEditMode(true);
-    setShowEditAppointment(true);
   };
 
   const handleEditWithoutId = () => {
--- src/components/AccountSection/AccountSection.tsx
+++ src/components/AccountSection/AccountSection.tsx
@@ -3,6 +3,10 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
 import { useSession } from 'next-auth/react';
 import { format, isToday } from 'date-fns';
+import Image from 'next/image';
+import { useQuery } from 'react-query';
+import { getAllAppointments } from '@/services/AppointmentAPI';
+import { getAllSessions } from '@/services/SessionsAPI';
 
 interface AccountSectionProps {
   photoUrl: string;
@@ -19,48 +23,69 @@ interface Session {
 
 const AccountSection: React.FC<AccountSectionProps> = ({ photoUrl }) => {
   const { data: session } = useSession();
-  const apiBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
   const [notificationCount, setNotificationCount] = useState(0);
+  const [todaySessions, setTodaySessions] = useState<Session[]>([]);
+  const [displaySessions, setDisplaySessions] = useState<boolean>(false);
 
-  const fetchSessions = async () => {
-    try {
-      const response = await fetch(`${apiBaseUrl}/admin/sessions`);
-      const data = await response.json();
+  const { data: appointments, refetch } = useQuery({
+    queryKey: ['artists'],
+    queryFn: getAllAppointments,
+    refetchOnWindowFocus: false,
+  });
 
-      const todayDate = format(new Date(), 'yyyy-MM-dd');
+  const { data: sessions, refetch: refetchSessions } = useQuery({
+    queryKey: ['sessions'],
+    queryFn: getAllSessions,
+    refetchOnWindowFocus: false,
+  });
 
-      const todaySessions = data.sessions.filter((session: Session) =>
+  const handleSessions = () => {
+    setTodaySessions(
+      sessions?.sessions.filter((session: Session) =>
         isToday(new Date(session.date)),
-      );
-
-      setNotificationCount(todaySessions.length);
-    } catch (error) {
-      console.error('Error al obtener sesiones:', error);
-    }
+      ),
+    );
+    setNotificationCount(todaySessions.length);
   };
 
   useEffect(() => {
-    fetchSessions();
-    const intervalId = setInterval(fetchSessions, 60 * 1000);
-    return () => clearInterval(intervalId);
+    handleSessions();
   }, []);
 
   return (
     <div className={styles.accountSection}>
       <div className={styles.notificationIcon}>
+        {notificationCount > 0 && displaySessions && (
+          <div className={styles.notificationSessions}>
+            {todaySessions.map((session: Session) => (
+              <div key={session.id} className={styles.notification}>
+                <p>{format(new Date(session.date), 'HH:mm')}</p>
+                <p>{session.appointment_id}</p>
+              </div>
+            ))}
+          </div>
+        )}
         <span>{notificationCount}</span>
-        <img
+        <Image
           className={styles.iconN}
           src="https://cdn.icon-icons.com/icons2/494/PNG/512/alarm_icon-icons.com_48364.png"
           alt="Notificaciones"
+          width={24}
+          height={24}
+          onClick={() => setDisplaySessions(!displaySessions)}
         />
       </div>
       <div className={styles.userInfo}>
         <span>{session?.user?.name}</span>
-        <img src={photoUrl} alt={`Foto de ${session?.user?.name}`} />
+        <Image
+          className={styles.userImage}
+          src={photoUrl}
+          alt="User Photo"
+          width={100}
+          height={100}
+        />
       </div>
     </div>
   );
 };
-
 export default AccountSection;
